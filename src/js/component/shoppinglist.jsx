26caa23820be0ae795ab2daccf18cff832109be2
Eduardo Rocha <134506270+eddyrocha78@users.noreply.github.com>
2023-09-05 08:10:16 +0000

    starting project

---
+++ src/js/component/shoppinglist.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ShoppingList = () => {
+	//const (listItem, setListItem) = useState('');
+
+	return (
+		<>
+		 <ul>
+			<li>Coffee</li>
+			<li>Tea</li>
+			<li>Milk</li>
+		</ul> 
+		</>
+	);
+};
+
+export default ShoppingList;
